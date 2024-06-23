0a0685715d23a632d6ab3cb419208058319af1ed
TDLofCC <TDLofCC@gmail.com>
2024-06-23 19:22:33 +0200

    Update summoner.go

--- internal/run/summoner.go
+++ internal/run/summoner.go
@@ -1,12 +1,12 @@
 package run
 
 import (
+	"slices"
+	"github.com/hectorgimenez/d2go/pkg/data/npc"
+	"github.com/hectorgimenez/koolo/internal/config"
 	"github.com/hectorgimenez/d2go/pkg/data"
 	"github.com/hectorgimenez/d2go/pkg/data/area"
-	"github.com/hectorgimenez/d2go/pkg/data/npc"
 	"github.com/hectorgimenez/koolo/internal/action"
-	"github.com/hectorgimenez/koolo/internal/config"
-	"github.com/hectorgimenez/koolo/internal/game"
 )
 
 type Summoner struct {
@@ -16,15 +16,45 @@ type Summoner struct {
 func (s Summoner) Name() string {
 	return string(config.SummonerRun)
 }
-
 func (s Summoner) BuildActions() (actions []action.Action) {
+	monsterFilter := data.MonsterAnyFilter()
 	return []action.Action{
+		s.builder.VendorRefill(true, false), // first time to buy keys
+		s.builder.VendorRefill(true, true),  // second time to force IDs/TPs
 		s.builder.WayPoint(area.ArcaneSanctuary), // Moving to starting point (Arcane Sanctuary)
-		s.builder.MoveTo(func(d game.Data) (data.Position, bool) {
-			m, found := d.NPCs.FindOne(npc.Summoner)
-
-			return m.Positions[0], found
-		}), // Travel to boss position
-		s.char.KillSummoner(), // Kill Summoner
+		s.builder.Buff(),
+		// Skip lightning spires
+		s.builder.ClearArea(true, func(m data.Monsters) []data.Monster {
+			var monsters []data.Monster
+			monsters = monsterFilter(m)
+			monsters = skipLightSpireFilter(monsters)
+			return monsters
+		}),
 	}
 }
+
+func skipLightSpireFilter(monsters data.Monsters) []data.Monster {
+	var lightSpireIds = []npc.ID{npc.LightningSpire}
+	var filteredMonsters []data.Monster
+
+	for _, m := range monsters {
+		if !slices.Contains(lightSpireIds, m.Name) {
+			filteredMonsters = append(filteredMonsters, m)
+		}
+	}
+
+	return filteredMonsters
+}
+
+// Original function
+//func (s Summoner) BuildActions() (actions []action.Action) {
+//	return []action.Action{
+//		s.builder.WayPoint(area.ArcaneSanctuary), // Moving to starting point (Arcane Sanctuary)
+//		s.builder.MoveTo(func(d game.Data) (data.Position, bool) {
+//			m, found := d.NPCs.FindOne(npc.Summoner)
+//
+//			return m.Positions[0], found
+//		}), // Travel to boss position
+//		s.char.KillSummoner(), // Kill Summoner
+//	}
+//}
